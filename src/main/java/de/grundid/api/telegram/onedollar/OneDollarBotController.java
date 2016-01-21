d101f0a40f7e30a735e59e7e4a76236f0ee2d116
Adrian Stabiszewski <as@grundid.de>
2016-01-21 18:26:18 +0100

    unicode

--- src/main/java/de/grundid/api/telegram/onedollar/OneDollarBotController.java
+++ src/main/java/de/grundid/api/telegram/onedollar/OneDollarBotController.java
@@ -18,7 +18,8 @@ import java.util.Arrays;
 @RestController
 public class OneDollarBotController {
 
-	private static final String GIVE_ONE_DOLLAR = "Give one dollar \\U1F4B0 \\U1F4B5";
+	private static final String GIVE_ONE_DOLLAR =
+			"Give one dollar " + Character.toString((char)0xF4B0); // \\U1 \\U1F4B5";
 
 	@RequestMapping(value = "/bot/onedollar", method = RequestMethod.POST)
 	public ResponseEntity<?> post(@RequestBody Update update) {
