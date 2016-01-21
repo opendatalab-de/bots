07618f27f5ce6db7964be45987b48f8f1ba31047
Lukas Himsel <lukas.himsel@web.de>
2016-01-21 21:33:13 +0100

    das alles vernichtende update

--- src/main/java/de/grundid/api/telegram/laboeuf/RandomShiaMessage.java
+++ src/main/java/de/grundid/api/telegram/laboeuf/RandomShiaMessage.java
@@ -11,8 +11,8 @@ public class RandomShiaMessage {
             "Make your dreams come true",
             "Yesterday you said tomorrow so JUST DO IT. NOTHING IS IMPOSSIBLE http://lukashimsel.me/pics/doit/1.png",
             "Some people dream success while you're gonna wake up and work harder. Nothing is impossible",
-            "You should get to the point where anyone else would quit you're not gonna stop there.http://lukashimsel.me/pics/doit/3.png",
-            "Know what you're waiting for to  DO IThttp://lukashimsel.me/pics/doit/4.png"
+            "You should get to the point where anyone else would quit you're not gonna stop there. http://lukashimsel.me/pics/doit/3.png",
+            "Know what you're waiting for to  DO IT http://lukashimsel.me/pics/doit/4.png"
     };
 
     public String getRandomShiaMessage(){
