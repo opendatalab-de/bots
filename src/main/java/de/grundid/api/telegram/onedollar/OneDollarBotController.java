1172d2b9226973bdc22b1d21220e3e84c2d209ac
Adrian Stabiszewski <as@grundid.de>
2016-01-21 18:12:56 +0100

    keyboard test

--- src/main/java/de/grundid/api/telegram/onedollar/OneDollarBotController.java
+++ src/main/java/de/grundid/api/telegram/onedollar/OneDollarBotController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.api.methods.SendMessage;
 import org.telegram.api.objects.Message;
+import org.telegram.api.objects.ReplyKeyboardMarkup;
 import org.telegram.api.objects.Update;
 
 import java.text.ParseException;
+import java.util.Arrays;
 
 @RestController
 public class OneDollarBotController {
@@ -21,28 +23,37 @@ public class OneDollarBotController {
 		Message message = update.getMessage();
 		SendMessage sendMessage = new SendMessage();
 		sendMessage.setChatId(message.getChatId());
+		ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+		keyboardMarkup.setOneTimeKeyboad(true);
+		keyboardMarkup.setResizeKeyboard(true);
+		keyboardMarkup
+				.setKeyboard(Arrays.asList(Arrays.asList("/give", "/give 2"), Arrays.asList("/give 3", "/give 4")));
+
 		if (StringUtils.hasText(message.getText())) {
 			try {
 				CommandParser commandParser = new CommandParser(message.getText());
 				String command = commandParser.getCommand();
 				if ("give".equals(command)) {
 					sendMessage.setText(
-							"[Give with PayPal](http://bit.ly/OneDollarBot)");
+							"[Give one dollar with PayPal](http://bit.ly/OneDollarBot)");
 					sendMessage.enableMarkdown(true);
 				}
 				else {
 					sendMessage.setText(
 							"Do you have some money for me?");
+					sendMessage.setReplayMarkup(keyboardMarkup);
 				}
 			}
 			catch (ParseException e) {
 				sendMessage.setText(
-						"Sorry.");
+						"Do you have some money for me?");
+				sendMessage.setReplayMarkup(keyboardMarkup);
 			}
 			return ResponseEntity.ok(sendMessage);
 		}
 		else {
 			sendMessage.setText("Hey. What's up?");
+			sendMessage.setReplayMarkup(keyboardMarkup);
 			return ResponseEntity.ok().body(sendMessage);
 		}
 	}
