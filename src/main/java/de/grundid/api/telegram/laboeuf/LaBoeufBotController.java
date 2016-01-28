d18daabbc0c2c24b842ff619870c4364095b75e2
Adrian Stabiszewski <as@grundid.de>
2016-01-28 19:09:42 +0100

    rating added

--- src/main/java/de/grundid/api/telegram/laboeuf/LaBoeufBotController.java
+++ src/main/java/de/grundid/api/telegram/laboeuf/LaBoeufBotController.java
@@ -19,52 +19,46 @@ import java.util.List;
 @RestController
 public class LaBoeufBotController {
 
-	private static Logger log = LoggerFactory.getLogger(LaBoeufBotController.class);
+    private static Logger log = LoggerFactory.getLogger(LaBoeufBotController.class);
 
 
-	@RequestMapping(value = "/bot/laboeuf", method = RequestMethod.POST)
-	public ResponseEntity<?> post(@RequestBody Update update) {
-		Message message = update.getMessage();
-		SendMessage sendMessage = new SendMessage();
-		sendMessage.setChatId(message.getChatId());
-		if(StringUtils.hasText(message.getText())){
-			log.info(message.getText());
-			try{
-				CommandParser commandParser = new CommandParser(message.getText());
-				String command = commandParser.getCommand();
-				List<String> params= commandParser.getParams();
-				if("START".equals(command.toUpperCase())){
-					sendMessage.setText("Just do it.");
-				}
-				else if("SHIA".equals(command.toUpperCase())){
-					RandomShiaMessage randomShiaMessage = new RandomShiaMessage();
-					sendMessage.setText(randomShiaMessage.getRandomShiaMessage());
-				}
-				else if("ABOUT".equals(command.toUpperCase())){
-					sendMessage.setText("About:\nThis bot is made to motivate you. The idea came, when I saw Shia Laboeuf's 'Just Do It' motivation speech. https://youtu.be/ZXsQAXx_ao0\nCreated By Lukas Himsel (http://lukashimsel.me)");
-				} else if ("RATE".equals(command.toUpperCase())) {
-					sendMessage.setText("Please rate my bot at [https://telegram.me/storebot?start=LaBoeuf_Bot](Store Bot)");
-				}
-				else if("HELP".equals(command.toUpperCase())){
-					sendMessage.setText("Commands:\n/start Start conversation\n/shia get a motivation message\n/about A message that describes why i made this bot\n/help this message");
-				}
-				else if("ARE".equals(command.toUpperCase()) && params.size()==2){
-					if("YOU".equals(params.get(0).toUpperCase()) && "FAMOUS?".equals(params.get(1).toUpperCase())){
-						sendMessage.setText("I'm not famous anymore.\nhttp://lukashimsel.me/pics/doit/famous.jpg");
-					}
-					else{
-						sendMessage.setText("unknown command");
-					}
-				}
-				else{
-					sendMessage.setText("unknown command");
-				}
-			} catch (ParseException e){
-				sendMessage.setText("unknown command");
-			}
-		} else{
-			sendMessage.setText("unknown command.");
-		}
-		return ResponseEntity.ok().body(sendMessage);
-	}
+    @RequestMapping(value = "/bot/laboeuf", method = RequestMethod.POST)
+    public ResponseEntity<?> post(@RequestBody Update update) {
+        Message message = update.getMessage();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        if (StringUtils.hasText(message.getText())) {
+            log.info(message.getText());
+            try {
+                CommandParser commandParser = new CommandParser(message.getText());
+                String command = commandParser.getCommand();
+                List<String> params = commandParser.getParams();
+                if ("START".equals(command.toUpperCase())) {
+                    sendMessage.setText("Just do it.");
+                } else if ("SHIA".equals(command.toUpperCase())) {
+                    RandomShiaMessage randomShiaMessage = new RandomShiaMessage();
+                    sendMessage.setText(randomShiaMessage.getRandomShiaMessage());
+                } else if ("ABOUT".equals(command.toUpperCase())) {
+                    sendMessage.setText("About:\nThis bot is made to motivate you. The idea came, when I saw Shia Laboeuf's 'Just Do It' motivation speech. https://youtu.be/ZXsQAXx_ao0\nCreated By Lukas Himsel (http://lukashimsel.me)");
+                } else if ("RATE".equals(command.toUpperCase())) {
+                    sendMessage.setText("Please rate my bot at [https://telegram.me/storebot?start=LaBoeuf_Bot](Store Bot)");
+                } else if ("HELP".equals(command.toUpperCase())) {
+                    sendMessage.setText("Commands:\n/start Start conversation\n/shia get a motivation message\n/about A message that describes why i made this bot\n/help this message");
+                } else if ("ARE".equals(command.toUpperCase()) && params.size() == 2) {
+                    if ("YOU".equals(params.get(0).toUpperCase()) && "FAMOUS?".equals(params.get(1).toUpperCase())) {
+                        sendMessage.setText("I'm not famous anymore.\nhttp://lukashimsel.me/pics/doit/famous.jpg");
+                    } else {
+                        sendMessage.setText("unknown command");
+                    }
+                } else {
+                    sendMessage.setText("unknown command");
+                }
+            } catch (ParseException e) {
+                sendMessage.setText("unknown command");
+            }
+        } else {
+            sendMessage.setText("unknown command.");
+        }
+        return ResponseEntity.ok().body(sendMessage);
+    }
 }
