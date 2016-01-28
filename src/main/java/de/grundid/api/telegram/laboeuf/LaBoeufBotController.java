c08acf5a9548f1d6df7a4eb5f4d58cde570a7de8
Adrian Stabiszewski <as@grundid.de>
2016-01-28 19:09:29 +0100

    rating added

--- src/main/java/de/grundid/api/telegram/laboeuf/LaBoeufBotController.java
+++ src/main/java/de/grundid/api/telegram/laboeuf/LaBoeufBotController.java
@@ -1,15 +1,15 @@
 package de.grundid.api.telegram.laboeuf;
 
 import de.grundid.api.telegram.CommandParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.telegram.api.methods.SendAudio;
 import org.telegram.api.methods.SendMessage;
-import org.telegram.api.methods.SendPhoto;
 import org.telegram.api.objects.Message;
 import org.telegram.api.objects.Update;
 
@@ -19,12 +19,16 @@ import java.util.List;
 @RestController
 public class LaBoeufBotController {
 
+	private static Logger log = LoggerFactory.getLogger(LaBoeufBotController.class);
+
+
 	@RequestMapping(value = "/bot/laboeuf", method = RequestMethod.POST)
 	public ResponseEntity<?> post(@RequestBody Update update) {
 		Message message = update.getMessage();
 		SendMessage sendMessage = new SendMessage();
 		sendMessage.setChatId(message.getChatId());
 		if(StringUtils.hasText(message.getText())){
+			log.info(message.getText());
 			try{
 				CommandParser commandParser = new CommandParser(message.getText());
 				String command = commandParser.getCommand();
@@ -38,6 +42,8 @@ public class LaBoeufBotController {
 				}
 				else if("ABOUT".equals(command.toUpperCase())){
 					sendMessage.setText("About:\nThis bot is made to motivate you. The idea came, when I saw Shia Laboeuf's 'Just Do It' motivation speech. https://youtu.be/ZXsQAXx_ao0\nCreated By Lukas Himsel (http://lukashimsel.me)");
+				} else if ("RATE".equals(command.toUpperCase())) {
+					sendMessage.setText("Please rate my bot at [https://telegram.me/storebot?start=LaBoeuf_Bot](Store Bot)");
 				}
 				else if("HELP".equals(command.toUpperCase())){
 					sendMessage.setText("Commands:\n/start Start conversation\n/shia get a motivation message\n/about A message that describes why i made this bot\n/help this message");
