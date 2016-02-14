2a7677bcb168dd6c67cc3b4839b08c132120e3bd
Adrian Stabiszewski <as@grundid.de>
2016-02-14 09:44:57 +0100

    birthday chat

--- src/main/java/de/grundid/api/telegram/onedollar/OneDollarBotController.java
+++ src/main/java/de/grundid/api/telegram/onedollar/OneDollarBotController.java
@@ -16,25 +16,40 @@ import org.telegram.api.objects.Update;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.Arrays;
+import java.util.*;
 
 @Controller
 public class OneDollarBotController {
 
     private static final char[] MONEY_BAG = Character.toChars(0x1F4B0);
+    private static final char[] SMILE_WITH_GLASSES = Character.toChars(0x1F60E);
     private static final String GIVE_ONE_DOLLAR = "I'll give you one dollar " + MONEY_BAG[0] + MONEY_BAG[1];
-    @Autowired private OneDollarDatabaseService oneDollarDatabaseService;
-    @Autowired private ImageUpdateService imageUpdateService;
+    private static final String YES_IM_CHRISTIAN =
+            "Yes, I'm Christian." + SMILE_WITH_GLASSES[0] + SMILE_WITH_GLASSES[1];
+    private static final String YES_IM_VERY_RICH =
+            "Yes, I'm very rich." + MONEY_BAG[0] + MONEY_BAG[1] + " " + MONEY_BAG[0] + MONEY_BAG[1] + " " + MONEY_BAG[0]
+                    + MONEY_BAG[1];
+    private static final String YES_IM_QUITE_RICH =
+            "Yes, I'm quite rich." + MONEY_BAG[0] + MONEY_BAG[1] + " " + MONEY_BAG[0] + MONEY_BAG[1];
+    private static final String NO_IM_NOT_RICH =
+            "No, I'm not rich." + MONEY_BAG[0] + MONEY_BAG[1] + " " + MONEY_BAG[0] + MONEY_BAG[1];
+    @Autowired
+    private OneDollarDatabaseService oneDollarDatabaseService;
+    @Autowired
+    private ImageUpdateService imageUpdateService;
+    private ReplyKeyboardMarkup areYouRich;
+    private ReplyKeyboardMarkup areYouChristian;
+    private Set<Integer> knownChristianChats = new HashSet<>();
 
     @RequestMapping(value = "/bot/onedollar", method = RequestMethod.POST)
     public ResponseEntity<?> post(@RequestBody Update update) throws IOException {
         Message message = update.getMessage();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(message.getChatId());
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-        keyboardMarkup.setOneTimeKeyboad(true);
-        keyboardMarkup.setResizeKeyboard(true);
-        keyboardMarkup.setKeyboard(Arrays.asList(Arrays.asList(GIVE_ONE_DOLLAR)));
+        ReplyKeyboardMarkup keyboardMarkup = createKeyboard(Arrays.asList(Arrays.asList(GIVE_ONE_DOLLAR)));
+        areYouChristian = createKeyboard(Arrays.asList(Arrays.asList("Yes, I'm Christian", "No.")));
+        areYouRich = createKeyboard(Arrays.asList(Arrays.asList(YES_IM_VERY_RICH), Arrays.asList(YES_IM_QUITE_RICH),
+                Arrays.asList("No, I'm not rich.")));
         if (StringUtils.hasText(message.getText())) {
             if (CommandParser.isCommand(message.getText())) {
                 try {
@@ -55,16 +70,24 @@ public class OneDollarBotController {
                 }
             }
             else {
+                System.out.println(message.getText());
                 if (GIVE_ONE_DOLLAR.equals(message.getText())) {
                     sendMessage.setText("[Give one dollar with PayPal](http://bit.ly/OneDollarBot)");
                     sendMessage.enableMarkdown(true);
                     oneDollarDatabaseService.addChatId(message.getChatId(), message.getFrom().getId());
                 }
                 else {
-                    sendMessage.setText("I have no money. You Chat Id: " + message.getChatId());
-                    sendMessage.setReplayMarkup(keyboardMarkup);
-                    imageUpdateService.sendPhoto("shia01.png", "Good luck", message.getChatId());
-                    return ResponseEntity.ok("");
+                    Calendar now = Calendar.getInstance();
+                    if (now.get(Calendar.MONTH) == Calendar.FEBRUARY && now.get(Calendar.DAY_OF_MONTH) == 14) {
+                        handleBirthday(sendMessage, message.getText());
+                        return ResponseEntity.ok(sendMessage);
+                    }
+                    else {
+                        sendMessage.setText("I have no money. You Chat Id: " + message.getChatId());
+                        sendMessage.setReplayMarkup(keyboardMarkup);
+                        imageUpdateService.sendPhoto("shia01.png", "Good luck", message.getChatId());
+                        return ResponseEntity.ok(sendMessage);
+                    }
                 }
             }
             return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(sendMessage);
@@ -74,5 +97,39 @@ public class OneDollarBotController {
             sendMessage.setReplayMarkup(keyboardMarkup);
             return ResponseEntity.ok(sendMessage);
         }
+    }
+
+    private void handleBirthday(SendMessage sendMessage, String message) {
+        if (YES_IM_CHRISTIAN.equals(message)) {
+            if (knownChristianChats.contains(sendMessage.getChatId())) {
+                sendMessage.setText("We have already talked, sir. Have a nice Birthday!");
+            }
+            else {
+                sendMessage.setText("Happy Birthday to you, sir. Are you rich?");
+                sendMessage.setReplayMarkup(areYouRich);
+            }
+        }
+        else if (YES_IM_VERY_RICH.equals(message)) {
+            sendMessage.setText("[Give five dollar with PayPal](http://bit.ly/OneDollarBot)");
+            sendMessage.enableMarkdown(true);
+        }
+        else if (YES_IM_QUITE_RICH.equals(message)) {
+        }
+        else if (NO_IM_NOT_RICH.equals(message)) {
+            sendMessage.setText("You should start making good Apps, sir. Talk to Adrian.");
+            knownChristianChats.add(sendMessage.getChatId());
+        }
+        else {
+            sendMessage.setText("Good day to you, sir. Are you Christian?");
+            sendMessage.setReplayMarkup(areYouChristian);
+        }
+    }
+
+    private ReplyKeyboardMarkup createKeyboard(List<List<String>> keyboard) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setOneTimeKeyboad(true);
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setKeyboard(keyboard);
+        return keyboardMarkup;
     }
 }
