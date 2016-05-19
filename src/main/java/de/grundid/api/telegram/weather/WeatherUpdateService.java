e0b1ee9d72cf687ff87a2877ec1ec4516a53eff9
Lukas Himsel <lukas.himsel@web.de>
2016-05-19 17:29:52 +0200

    nicht mein server ...

--- src/main/java/de/grundid/api/telegram/weather/WeatherUpdateService.java
+++ src/main/java/de/grundid/api/telegram/weather/WeatherUpdateService.java
@@ -31,12 +31,21 @@ public class WeatherUpdateService {
     private long lastCheck = System.currentTimeMillis();
     private Integer chatId;
     private Map<String, String> sensors = new HashMap<>();
+    private Map<String, Boolean> ctrl = new HashMap<>();
+
     public WeatherUpdateService(){
         sensors.put("cowo.outside.temperature", "Außentemperatur");
         sensors.put("cowo.inside1.temperature", "Innentemperatur Süd");
         sensors.put("cowo.inside2.temperature", "Innentemperatur Nord");
         sensors.put("cowo.inside2.humidity", "Luftfeuchtigkeit Innen");
         sensors.put("cowo.outside.humidity", "Luftfeuchtigkeit Außen");
+
+        ctrl.put("cowo.outside.temperature", false);
+        ctrl.put("cowo.inside1.temperature", false);
+        ctrl.put("cowo.inside2.temperature", false);
+        ctrl.put("cowo.inside2.humidity", false);
+        ctrl.put("cowo.outside.humidity", false);
+
     }
 
     @Scheduled(fixedDelay = 5 * 60 * 1000, initialDelay = 20 * 1000)
@@ -46,32 +55,42 @@ public class WeatherUpdateService {
     }
 
     public void forceUpdateSince(long lastCheck) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.enableMarkdown(true);
-        sendMessage.setChatId(chatId);
         String messageContent = getStatus();
-        sendMessage.setText(messageContent);
-        try {
-            ResponseEntity<String> responseEntity = restTemplate
-                    .postForEntity(Constants.BASEURL + apiKey + "/" + SendMessage.PATH, sendMessage, String.class);
-        }
-        catch (RestClientException e) {
-            throw new IllegalStateException(e);
+        if(messageContent!=null){
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.enableMarkdown(true);
+            sendMessage.setChatId(chatId);
+
+            sendMessage.setText(messageContent);
+            try {
+                ResponseEntity<String> responseEntity = restTemplate
+                        .postForEntity(Constants.BASEURL + apiKey + "/" + SendMessage.PATH, sendMessage, String.class);
+            }
+            catch (RestClientException e) {
+                throw new IllegalStateException(e);
+            }
         }
     }
 
     public String getStatus() {
         StringBuilder stringBuilder = new StringBuilder();
+        boolean valTooOld=false;
         for (Map.Entry<String, String> entry : sensors.entrySet()) {
             de.grundid.api.telegram.weather.PagedResponse pagedResponse = restTemplate
                     .getForObject("http://api.grundid.de/sensor?sensorName=" + entry.getKey() + "&size=1", de.grundid.api.telegram.weather.PagedResponse.class, lastCheck);
 
             if (pagedResponse != null && pagedResponse.getContent() != null && !pagedResponse.getContent().isEmpty()) {
-                long diff = (System.currentTimeMillis() - pagedResponse.getContent().get(0).getTimestamp()) / 60 / 1000;
-                stringBuilder.append("Letzter Wert von " + entry.getValue() + " ist " + diff + " Minuten her\n");
+                long diff = (System.currentTimeMillis() - pagedResponse.getContent().get(0).getTimestamp());
+                if(diff > (15 * 60 * 1000)){
+                    if(ctrl.get(entry.getKey())==false){
+                        valTooOld=true;
+                        ctrl.put(entry.getKey(), true);
+                        stringBuilder.append("Letzter Wert von " + entry.getValue() + " ist " + (diff / 1000 / 60) + " Minuten her\n");
+                    }
+                }
             }
         }
-        return stringBuilder.toString();
+        return (valTooOld==true) ? stringBuilder.toString() : null;
     }
 
     public void setChatId(Integer chatId) {
