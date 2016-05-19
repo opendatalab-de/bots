41872a4e8588efccee5a6022a495d17b252b0f28
Adrian Stabiszewski <as@grundid.de>
2016-05-19 15:38:55 +0200

    added chat database

--- src/main/java/de/grundid/api/telegram/weather/WeatherBotController.java
+++ src/main/java/de/grundid/api/telegram/weather/WeatherBotController.java
@@ -1,7 +1,8 @@
 package de.grundid.api.telegram.weather;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.grundid.api.telegram.CommandParser;
-import de.grundid.api.telegram.laboeuf.RandomShiaMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,6 @@ import org.telegram.api.objects.Message;
 import org.telegram.api.objects.Update;
 
 import java.text.ParseException;
-import java.util.List;
 
 /**
  * Created by lukas on 19.05.16.
@@ -24,30 +24,43 @@ import java.util.List;
 @RestController
 public class WeatherBotController {
 
+    private static Logger log = LoggerFactory.getLogger(WeatherBotController.class);
     @Autowired
     private WeatherUpdateService weatherUpdateService;
+    @Autowired
+    private WeatherDatabaseService databaseService;
 
     @RequestMapping(value = "/bot/cowoWeather", method = RequestMethod.POST)
     public ResponseEntity<?> post(@RequestBody Update update) {
+        try {
+            log.info(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(update));
+        }
+        catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         Message message = update.getMessage();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(message.getChatId());
         weatherUpdateService.setChatId(message.getChatId());
-
         if (StringUtils.hasText(message.getText())) {
             try {
                 CommandParser commandParser = new CommandParser(message.getText());
                 String command = commandParser.getCommand();
-
                 if ("start".equals(command)) {
                     sendMessage.setText("start service.");
-                } else {
+                }
+                if ("register".equals(command)) {
+                    databaseService.addChatId(message.getChatId());
+                }
+                else {
                     sendMessage.setText("unknown command");
                 }
-            } catch (ParseException e) {
+            }
+            catch (ParseException e) {
                 sendMessage.setText("unknown command");
             }
-        } else {
+        }
+        else {
             sendMessage.setText("unknown command.");
         }
         return ResponseEntity.ok().body(sendMessage);
@@ -56,7 +69,8 @@ public class WeatherBotController {
     private String getUserName(Message message) {
         if (message.getFrom() != null && message.getFrom().getUserName() != null) {
             return message.getFrom().getUserName();
-        } else {
+        }
+        else {
             return "unknown username";
         }
     }
