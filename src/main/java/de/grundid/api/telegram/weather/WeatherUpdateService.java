951384a440841c2e3252cf42857ad5eec72009fc
Lukas Himsel <lukas.himsel@web.de>
2016-05-19 16:16:50 +0200

    get data from all sensors

--- src/main/java/de/grundid/api/telegram/weather/WeatherUpdateService.java
+++ src/main/java/de/grundid/api/telegram/weather/WeatherUpdateService.java
@@ -17,9 +17,7 @@ import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 /**
  * Created by lukas on 19.05.16.
@@ -32,6 +30,14 @@ public class WeatherUpdateService {
     private RestTemplate restTemplate = new RestTemplate();
     private long lastCheck = System.currentTimeMillis();
     private Integer chatId;
+    private Map<String, String> sensors = new HashMap<>();
+    public WeatherUpdateService(){
+        sensors.put("cowo.outside.temperature", "Außentemperatur");
+        sensors.put("cowo.inside1.temperature", "Innentemperatur Süd");
+        sensors.put("cowo.inside2.temperature", "Innentemperatur Nord");
+        sensors.put("cowo.inside2.humidity", "Luftfeuchtigkeit Innen");
+        sensors.put("cowo.outside.humidity", "Luftfeuchtigkeit Außen");
+    }
 
     @Scheduled(fixedDelay = 5 * 60 * 1000, initialDelay = 20 * 1000)
     public void updateDrinks() {
@@ -40,28 +46,32 @@ public class WeatherUpdateService {
     }
 
     public void forceUpdateSince(long lastCheck) {
-        de.grundid.api.telegram.weather.PagedResponse pagedResponse = restTemplate
-                .getForObject("http://api.grundid.de/sensor?sensorName=cowo.outside.temperature&size=1", de.grundid.api.telegram.weather.PagedResponse.class,
-                        lastCheck);
-        if (pagedResponse != null && pagedResponse.getContent() != null && !pagedResponse.getContent().isEmpty()) {
-
-            long diff = System.currentTimeMillis() - pagedResponse.getContent().get(0).getTimestamp();
-
-
-            SendMessage sendMessage = new SendMessage();
-            sendMessage.enableMarkdown(true);
-            sendMessage.setChatId(chatId);
-            String messageContent = "Der letzte Wert ist " + diff + " Minuten her";
-            sendMessage.setText(messageContent);
-            try {
-                ResponseEntity<String> responseEntity = restTemplate
-                        .postForEntity(Constants.BASEURL + apiKey + "/" + SendMessage.PATH, sendMessage, String.class);
-            }
-            catch (RestClientException e) {
-                 throw new IllegalStateException(e);
-            }
-
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        String messageContent = getStatus();
+        sendMessage.setText(messageContent);
+        try {
+            ResponseEntity<String> responseEntity = restTemplate
+                    .postForEntity(Constants.BASEURL + apiKey + "/" + SendMessage.PATH, sendMessage, String.class);
         }
+        catch (RestClientException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public String getStatus() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, String> entry : sensors.entrySet()) {
+            de.grundid.api.telegram.weather.PagedResponse pagedResponse = restTemplate
+                    .getForObject("http://api.grundid.de/sensor?sensorName=" + entry.getKey() + "&size=1", de.grundid.api.telegram.weather.PagedResponse.class, lastCheck);
+
+            if (pagedResponse != null && pagedResponse.getContent() != null && !pagedResponse.getContent().isEmpty()) {
+                long diff = (System.currentTimeMillis() - pagedResponse.getContent().get(0).getTimestamp()) / 60 / 1000;
+                stringBuilder.append("Letzter Wert von " + entry.getValue() + " ist " + diff + " Minuten her\n");
+            }
+        }
+        return stringBuilder.toString();
     }
 
     public void setChatId(Integer chatId) {
